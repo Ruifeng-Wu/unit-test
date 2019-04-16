68d20784e120b6d924656042d1a133a2c12e5165
Ruifeng-Wu <296078293@qq.com>
2019-04-16 17:05:39 +0800

    test:AnswerGeneratorTest

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,36 @@
 package tw.core.generator;
 
+import org.junit.Test;
+import tw.core.Answer;
+import tw.core.exception.OutOfRangeAnswerException;
+
+import java.lang.reflect.Field;
+
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
 
+    @Test
+    public void constructor_test() throws Exception {
+        RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
+        AnswerGenerator answerGenerator = new AnswerGenerator(randomIntGenerator);
+        Field field = AnswerGenerator.class.getDeclaredField("randomIntGenerator");
+        field.setAccessible(true);
+        Object obj = field.get(answerGenerator);
+        assertTrue(obj instanceof RandomIntGenerator);
+        assertSame(randomIntGenerator, obj);
+    }
+
+    @Test
+    public void generate_return_answer() throws Exception {
+        RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
+        AnswerGenerator answerGenerator = new AnswerGenerator(randomIntGenerator);
+        Object obj = answerGenerator.generate();
+        assertTrue("should return an object of type Answer",obj instanceof Answer);
+    }
 }
 
